2c0a09abef84876393c590e5e3eece1806d0fd5d
Matthew Brown <mjbme10@yahoo.com>
2022-04-12 14:03:20 -0500

    Made fixes and confirmed a working deployment

--- main.tf
+++ main.tf
@@ -10,17 +10,17 @@ terraform {
 provider "aws" {
   region = "us-east-1"
   #Using credentials file
-  shared_credentials_file = "credentials"
-  profile                 = "default"
+  shared_credentials_files = ["credentials"]
+  profile                  = "default"
 }
 
 ### Networking
-resource "aws_vpc" "main-vpc-vpc" {
+resource "aws_vpc" "main-vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_internet_gateway" "igw-1" {
-  vpc_id = aws_vpc.main-vpc-vpc.id
+  vpc_id = aws_vpc.main-vpc.id
   tags = {
     Name = "Internet Gateway 1"
   }
@@ -29,7 +29,7 @@ resource "aws_internet_gateway" "igw-1" {
 resource "aws_subnet" "publicsubnet1" { # Creating Public Subnets
   vpc_id            = aws_vpc.main-vpc.id
   cidr_block        = "10.0.1.0/24" # CIDR block of public subnets
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "public-subnet-1"
   }
@@ -38,7 +38,7 @@ resource "aws_subnet" "publicsubnet1" { # Creating Public Subnets
 resource "aws_subnet" "privatesubnet1" { # Creating Private Subnets
   vpc_id            = aws_vpc.main-vpc.id
   cidr_block        = "10.0.2.0/24" # CIDR block of private subnets
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "private-subnet-1"
   }
